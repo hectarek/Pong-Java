aca39d1982160f5f106092db517638e8153104ef
Hector Gonzalez <hectarek13@gmail.com>
2020-06-05 15:59:10 -0400

    figured out issue

--- Window.java
+++ Window.java
@@ -1,8 +1,5 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.BorderFactory;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Window {
    
@@ -10,11 +7,6 @@ public class Window {
 
         JFrame frame = new JFrame(title);
 
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        panel.setLayout(new GridLayout(0,1));
-
-        frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // frame.setResizable(false);
         frame.add(game);
